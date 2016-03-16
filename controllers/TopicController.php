016ddc983a1d5d30558589b205fbbac97c5374fc
forecho <caizhenghai@gmail.com>
2016-03-16 17:17:37 +0800

    【2.1】搭建最基本的 RESTful 架构

---
+++ controllers/TopicController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * author     : forecho <caizhenghai@gmail.com>
+ * createTime : 2016/3/16 16:59
+ * description:
+ */
+
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+class TopicController extends ActiveController
+{
+    public $modelClass = 'app\models\Topic';
+}
